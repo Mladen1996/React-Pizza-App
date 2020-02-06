0abd3e0286b9bbf8716713fd9da8ad346e3fb4fe
Mladen Živojinović <mladenzivojinovic96@gmail.com>
2020-02-06 18:43:57 +0100

    FoodDialog functioning with Hooks

--- src/Menu/Menu.js
+++ src/Menu/Menu.js
@@ -8,14 +8,16 @@ export const MenuStyled= styled.div`
     height:1000px;
     margin:0px 400px 50px 20px`;
 
-export default function Menu(){
+export default function Menu({setOpenFood}){
     return (<MenuStyled>
         { Object.entries(foods).map(([sectionName,foods]) => (
             <>
         <h1>{sectionName}</h1>
         <FoodGrid>
         {foods.map(food=>(
-            <Food img={food.img}>
+            <Food img={food.img} onClick={()=>{
+                setOpenFood(food);
+            }}>
                 <FoodLabel>
                 {food.name}
                 </FoodLabel>

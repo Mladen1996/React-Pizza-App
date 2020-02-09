71e3b5cd89d3592b372195c9daf7c4811e5a50c4
Mladen Živojinović <mladenzivojinovic96@gmail.com>
2020-02-09 19:09:36 +0100

    List Orders

--- src/Оrder/Order.js
+++ src/Оrder/Order.js
@@ -18,14 +18,28 @@ const OrderContent=styled(DialogContent)`
 padding:20px;
 height:100%;
 `;
+const OrderContainer=styled.div`
+padding:10px 0px;
+border-bottom:1px solid grey;
+`;
+const OrderItem=styled.div`
+padding:10px 0px;
+`;
 export default function Order({orders}){
     return <OrderStyled>
-     {orders.length===0 ?
-        <OrderContent>
-        Your order's looking pretty empty.
-        </OrderContent>:
-<OrderContent>Found {orders.length} orders</OrderContent>
-         }
+     {orders.length===0 ? (
+        <OrderContent> Your order's looking pretty empty.</OrderContent>)
+        : (
+<OrderContent>
+    {""}
+    <OrderContainer>Your Order:</OrderContainer>{""}
+    {orders.map(order=>(
+        <OrderContainer>
+            <OrderItem>{order.name}</OrderItem>
+        </OrderContainer>
+    ))}
+    </OrderContent>
+     )}
         <DialogFooter>
             <ConfirmButton>
                 Checkout

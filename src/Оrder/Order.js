89c7cfd7d65e146d6e229f95ddb1f29572921d5e
Mladen Živojinović <mladenzivojinovic96@gmail.com>
2020-02-16 15:32:01 +0100

    Add total,tax and subtotal

--- src/Оrder/Order.js
+++ src/Оrder/Order.js
@@ -31,6 +31,11 @@ grid-template-columns:20px 150px 20px 60px;
 justify-content:space-between;
 `;
 export default function Order({orders}){
+    const subtotal=orders.reduce((total,order)=>{
+        return total+ getPrice(order);
+    },0);
+    const tax=subtotal*0.07;
+    const total=subtotal+tax;
     return <OrderStyled>
      {orders.length===0 ? (
         <OrderContent> Your order's looking pretty empty.</OrderContent>)
@@ -47,6 +52,39 @@ export default function Order({orders}){
                 </OrderItem>
         </OrderContainer>
     ))}
+    <OrderContainer>
+        <OrderItem>
+            <div/>
+            <div>
+                Subtotal:
+            </div>
+            <div>
+                {formatPrice(subtotal)}
+            </div>
+        </OrderItem>
+        <OrderItem>
+            <div/>
+           
+            <div>
+                Tax: 
+            </div>
+            <div>
+                 {formatPrice(tax)}
+            </div>
+           
+        </OrderItem>
+        <OrderItem>
+            <div/>
+            <div>
+                Total: 
+            </div>
+            <div>
+                 {formatPrice(total)}
+            </div>
+           
+           
+        </OrderItem>
+    </OrderContainer>
     </OrderContent>
      )}
         <DialogFooter>

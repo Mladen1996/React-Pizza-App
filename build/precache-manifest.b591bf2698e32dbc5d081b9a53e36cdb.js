755bc01a711c4b99a28ce68a982e1ad2e100cb2e
Mladen Živojinović <mladenzivojinovic96@gmail.com>
2020-02-19 17:34:26 +0100

    Add Firebase

---
+++ build/precache-manifest.b591bf2698e32dbc5d081b9a53e36cdb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac1ee39a3af4241e748813a65f8a9907",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a7f4cee7e630d270378",
+    "url": "/static/css/main.b81aef37.chunk.css"
+  },
+  {
+    "revision": "c86f4dd2dbe272e7ddae",
+    "url": "/static/js/2.5778ae2e.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.5778ae2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a7f4cee7e630d270378",
+    "url": "/static/js/main.43b56c48.chunk.js"
+  },
+  {
+    "revision": "0a09fa3a4358bc3aa1ef",
+    "url": "/static/js/runtime-main.210d7def.js"
+  },
+  {
+    "revision": "593fda078040db0461c273a45ef6330d",
+    "url": "/static/media/banner.593fda07.jpg"
+  }
+]);

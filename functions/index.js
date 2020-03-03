7b75870f562721d97362722ef70c53c114b254d3
Mladen1996 <mladjalug96@gmail.com>
2020-03-03 16:14:22 +0100

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -5,6 +5,7 @@ const {email, password} = require('./config');
 
 const mailTransport=nodemailer.createTransport(
     {
+        service:'gmail',
         host: 'smtp.gmail.com',
         port: 465,
         secure: true,
